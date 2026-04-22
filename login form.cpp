72e58b7511084455db4c5d4384414aba466f4d2c
Dhawal-N-Katre <katredhawal9211@gmail.com>
2026-04-22 22:35:50 +0530

    Add HTML login form with styling

---
+++ login form.cpp
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.a{
+			width: 300px;
+			text-align: center;
+			border-style: solid;
+			margin: 100px auto;
+			border-radius: 10px;
+			padding: 20px;
+			background: white;
+			box-shadow: 0px 0px 10px grey;
+
+		}
+		button{
+			padding: 10px 20px;
+    		background: blue;
+    		color: white;
+   			 border: none;
+   			 width: 98%;
+   			 border-radius: 3px;
+		}
+		input{ 
+			width: 90%;
+    		padding: 10px;
+    		margin: 10px 0;
+
+		}
+	</style>
+</head>
+<body>
+<div class="a">
+	<h2>Login</h2>
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <button>Login</button>
+</div>
+</body>
+</html>
